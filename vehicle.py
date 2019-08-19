33a602456f51e690a1ea97859f68c069757b0b2a
StefanoRUsai <stefano.r.usai@gmail.com>
2019-08-19 15:35:29 +0200

    modifiche vehicle e utils

--- vehicle.py
+++ vehicle.py
@@ -3,6 +3,8 @@ class Vehicle:
         self.route = route
         self.weight = weight
         self.cost = cost
+        self.delivery = 0
+        self.pickUp = 0
         
     def add_route(self, b):
         if self.route[-1] == b[0]:
@@ -11,7 +13,13 @@ class Vehicle:
             self.route.append(b[0])
     
     def add_weight(self, weight):
-        self.weight+=weight
+        self.weight += weight
         
     def add_cost(self, cost):
-        self.cost+=cost
+        self.cost += cost
+    
+    def set_delivery(self, value):
+        self.delivery = value
+        
+    def set_pickUp(self, value):
+        self.pickUp = value
